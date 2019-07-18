9f8ad870da94058249f5e03fa5dcd057e8cb9643
Nathan Beck <njosefbeck@gmail.com>
2019-07-17 20:27:11 -0500

    Make logo link to /artists

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Logo = () => (
   <h1 className='logo'>
-    <Link to='/'>
+    <Link to='/artists'>
       Filipino American Artist Directory
     </Link>
   </h1>
